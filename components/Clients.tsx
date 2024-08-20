9e4821f753503f494f4620dc31b2d63219467739
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-08-21 02:05:41 +0530

    add : client companies ui

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
-import { testimonials } from "@/data";
+import { companies, testimonials } from "@/data";
 const Clients = () => {
   return (
-    <div className="py-20" id="projects">
+    <div className="py-20" id="testimonials">
       <h1 className="heading">
         Kind words from <span className="text-purple"> satisfied clients</span>
       </h1>
@@ -15,8 +15,25 @@ const Clients = () => {
           speed="slow"
         />
       </div>
-
-      
+      <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+        {companies.map((company) => (
+          <React.Fragment key={company.id}>
+            <div className="flex md:max-w-60 max-w-32 gap-2">
+              <img
+                src={company.img}
+                alt={company.name}
+                className="md:w-10 w-5"
+              />
+              <img
+                src={company.nameImg}
+                alt={company.name}
+                width={company.id === 4 || company.id === 5 ? 100 : 150}
+                className="md:w-24 w-20"
+              />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
